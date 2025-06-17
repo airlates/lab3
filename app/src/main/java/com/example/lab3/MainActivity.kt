00a7d58696ea39b94d006fe8a8bd4bcc80d3adae
airlates <rakov1337@gmail.com>
2025-06-18 02:03:14 +0700

    3333

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -5,9 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val frirstStringInput = findViewById<EditText>(R.id.firstStringInput)
+        val secondStringInput = findViewById<EditText>(R.id.secondStringInput)
+        val concatButton = findViewById<Button>(R.id.concatButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        concatButton.setOnClickListener {
+            val firstString = frirstStringInput.text.toString()
+            val secondString = secondStringInput.text.toString()
+
+            // Дозаписываем вторую строку в конец первой
+            val combinedString = frirstString + secondString
+
+            resultTextView.text = "Результат: $combinedString"
         }
     }
 }
